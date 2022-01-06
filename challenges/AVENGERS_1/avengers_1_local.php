86e8b9c145fc775ce84fcf4ee48f48de2ce0ae3c
Arthur Weill <a.weill@webandcow.com>
2022-01-06 16:54:56 +0100

    Solution AVENGERS_1 - V1
    Avec des booléens et des if/else/if

--- challenges/AVENGERS_1/avengers_1_local.php
+++ challenges/AVENGERS_1/avengers_1_local.php
@@ -18,8 +18,54 @@ Html::debug($thanos, '$thanos');
 
 // CODE DU CHALLENGE ------------------
 
+$tour = 0;
+$puissanceAvengers = 0;
 
+$tourIronMan = true;
+$tourSpiderman = false;
+$tourCaptainamerica = false;
+$tourThor = false;
 
+while ($puissanceAvengers <= $thanos) {
+
+	// Calcul puissance Avengers
+	$puissanceIronMan = $ironman * 3 + 10;
+	$puissanceSpiderman = $spiderman * 4 + 5;
+	$puissanceCaptainamerica = $captainamerica * 3 + 7;
+	$puissanceThor = $thor * 4 + 20;
+
+	$puissanceAvengers = $puissanceIronMan + $puissanceSpiderman + $puissanceCaptainamerica + $puissanceThor;
+
+	Html::debug($puissanceAvengers, 'Puissance Avengers');
+
+	$tour++;
+
+	// D'abord ironman
+	if ($tourIronMan) {
+		$ironman++;
+		$tourIronMan = false;
+		$tourSpiderman = true;
+
+	// Puis Spiderman
+	} elseif ($tourSpiderman) {
+		$spiderman++;
+		$tourSpiderman = false;
+		$tourCaptainamerica = true;
+
+	// Puis Captain America
+	} elseif ($tourCaptainamerica) {
+		$captainamerica++;
+		$tourCaptainamerica = false;
+		$tourThor = true;
+
+	// Puis Thor
+	} elseif ($tourThor) {
+		$thor++;
+		$tourThor = false;
+		$tourIronMan = true;
+	}
+}
 
 // REPONSE ATTENDUE -------------------
+Html::debug($tour, 'Tours trouvés');
 Html::debug('55', 'Réponse attendue', 'success');
